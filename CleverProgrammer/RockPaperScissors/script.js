015da6c225fa03d24763461f73031ebec708962c
unknown <ashish.pradhan.e21@nsut.ac.in>
2021-09-13 17:22:52 +0530

    RPS Game

---
+++ CleverProgrammer/RockPaperScissors/script.js
@@ -0,0 +1,11 @@
+function rpsGame(yourChoice) {
+    console.log(yourChoice);
+    console.log(yourChoice.id);
+    var humanChoice, botChoice;
+    // humanChoice = yourChoice.id;
+    // botChoice = 
+    // results = decideWinner(humanChoice, botChoice);
+    // message = finalMessage(results);
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+
+}
